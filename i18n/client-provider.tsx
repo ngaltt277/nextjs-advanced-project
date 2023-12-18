035ab809dce4d544c9e56a1401ea577ff69bfc6e
ngaltt277 <lttn2772001@gmail.com>
2023-12-18 10:57:21 +0700

    .

--- i18n/client-provider.tsx
+++ i18n/client-provider.tsx
@@ -6,14 +6,13 @@ import { Many } from "lodash";
 
 type Props = {
   children: ReactNode;
-  message: Many<string>[] | string;
 };
 
-export default async function ClientProvider({ children, message }: Props) {
+export default async function ClientProvider({ children }: Props) {
   const messages = await getMessages();
   
   return (
-    <NextIntlClientProvider messages={pick(messages, message)}>
+    <NextIntlClientProvider messages={messages}>
       {children}
     </NextIntlClientProvider>
   );
